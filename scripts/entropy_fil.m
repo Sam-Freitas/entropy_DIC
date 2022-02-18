dcf2d71b6d38be1b0debccab3b553a43a4706f8d
Sam-Freitas <samfreitas@email.arizona.edu>
2022-02-18 16:49:19 -0700

    update

--- scripts/entropy_fil.m
+++ scripts/entropy_fil.m
@@ -7,14 +7,32 @@ for i = 1:length(img_paths)
     
     imgs{i} = imread(fullfile(imgs_dir,img_paths(i).name));
     
-    E_imgs{i} = entropyfilt(imgs{i});
+    E_imgs{i} = entropyfilt(imgs{i},true(15));
     
 end
 
 large_E_array = imtile(E_imgs);
 [N,edges] = histcounts(nonzeros(large_E_array));
 
-smooth_N = smooth(N);
+smooth_N = smooth(N,10,'rloess');
+
+[TF,P] = islocalmin(smooth_N);
+
+inflection_point = find(P==max(P));
 
 e2 = edges(1:end-1);
 N_1000 = N>1000;
+
+figure;
+plot(e2,smooth_N,'b-',e2(inflection_point),smooth_N(inflection_point),'r*')
+
+E_sep_point = e2(inflection_point);
+
+for i = 1:length(img_paths)
+    
+    amount_open(i) = sum(sum(E_imgs{i}<E_sep_point));
+    
+end
+
+figure;
+plot(amount_open/max(amount_open))
