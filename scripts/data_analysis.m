998f74c009ee307d667b10e4d67b5c428177d1fa
Sam-Freitas <samfreitas@email.arizona.edu>
2022-02-21 16:14:46 -0700

    simple data analysis script

---
+++ scripts/data_analysis.m
@@ -0,0 +1,59 @@
+clear all
+close all force hidden
+
+num_conditions = 8;
+
+ovr_dir = fullfile(pwd,'output');
+
+if ispc
+    [~,message,~] = fileattrib([ovr_dir,'\*']);
+else
+    [~,message,~] = fileattrib([ovr_dir,'/*']);
+end
+
+fprintf('\nThere are %i total files & folders in the overarching folder.\n',numel(message));
+
+allExts = cellfun(@(s) s(end-2:end), {message.Name},'uni',0); % Get exts
+
+CSVidx = ismember(allExts,'csv');    % Search ext for "CSV" at the end
+CSV_filepaths = {message(CSVidx).Name}';  % Use CSVidx to list all paths.
+
+fprintf('There are %i files with *.CSV exts.\n',numel(CSV_filepaths));
+
+CSV_filepaths = natsort(CSV_filepaths);
+
+kmeans_idx = sort(kmeans(char(string(CSV_filepaths)),num_conditions));
+
+hold on
+for i = 1:num_conditions
+    
+    these_csvs = CSV_filepaths(kmeans_idx==i);
+    
+    [~,this_condition,~] = fileparts(these_csvs{1});
+    condition{i} = replace(this_condition(2:end-7),'_','-');
+    
+    for j = 1:length(these_csvs)
+        temp_table = readtable(these_csvs{j},'VariableNamingRule','preserve');
+        if j == 1
+            norm_data = temp_table.("Norm open");
+        else
+            norm_data = [norm_data,temp_table.("Norm open")];
+        end
+    end
+    
+    avg_norm_data = medfilt1(mean(norm_data,2),5);
+    
+    plot(1:length(avg_norm_data),avg_norm_data,std(norm_data,0,2));
+    
+end
+legend(condition)
+
+
+
+
+
+
+
+
+
+
