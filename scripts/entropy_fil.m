3e1d5ffab7899560e584515d5985bea74a562c2c
Sam-Freitas <samfreitas@email.arizona.edu>
2022-02-21 14:59:05 -0700

    major code update

--- scripts/entropy_fil.m
+++ scripts/entropy_fil.m
@@ -1,58 +1,138 @@
+clear all
+close all force hidden
+warning('off', 'MATLAB:MKDIR:DirectoryExists');
 
 % imgs_dir = "Y:\Users\Raul Castro\Microscopes\Olympus Spining Disk\2022-02-16\ImageJ processed\RENCA_A28_5FBS_3HAA_1";
-containing_folder = "/Volumes/Sutphin server/Users/Raul Castro/Microscopes/Olympus Spining Disk/2022-02-16/ImageJ processed";
+% containing_folder = "/Volumes/Sutphin server/Users/Raul Castro/Microscopes/Olympus Spining Disk/2022-02-16/ImageJ processed";
+containing_folder = "Y:\Users\Raul Castro\Microscopes\Olympus Spining Disk\2022-02-16\ImageJ processed";
 ovr_dir = dir(containing_folder);
 ovr_dir(ismember( {ovr_dir.name}, {'.', '..'})) = [];  %remove . and ..
 
-imgs_dir = fullfile(ovr_dir(1).folder,ovr_dir(1).name);
+export_images = 1;
 
-img_paths = dir(fullfile(imgs_dir,'*.tif'));
-
-progress_bar = 0;
-for i = 1:length(img_paths)
-    progress_bar = progressbar_function(i,length(img_paths),progress_bar);
-    imgs{i} = imread(fullfile(imgs_dir,img_paths(i).name));
-    E_imgs{i} = entropyfilt(imgs{i},true(15));
-    if isequal(i,length(img_paths))
-        close_progressbar(progress_bar)
+mkdir('output');
+for i = 1:length(ovr_dir)
+        
+    this_exp = ovr_dir(i).name;
+    this_exp_display = replace(this_exp,'_','-');
+    
+    imgs_dir = fullfile(ovr_dir(1).folder,this_exp);
+    
+    img_paths = dir(fullfile(imgs_dir,'*.tif'));
+    sorted_img_paths = natsort({img_paths.name});
+    
+    num_imgs = length(sorted_img_paths);
+    
+    progress_bar = 0;
+    for j = 1:num_imgs
+        progress_bar = progressbar_function(j,num_imgs,progress_bar,{'Processing data',char(this_exp_display)});
+        % read in image
+        imgs{j} = imread(fullfile(imgs_dir,sorted_img_paths{j}));
+        % entropy filter the image for amount of disturbance
+        E_imgs{j} = entropyfilt(imgs{j},true(15));
+        if isequal(j,num_imgs)
+            % combine all the entropy images
+            large_E_array = imtile(E_imgs);
+            % get a histogram of the all the counts of the entropy
+            [N,edges] = histcounts(nonzeros(large_E_array));
+            close_progressbar(progress_bar)
+        end
     end
+    
+    % this get the segmentation threshold  
+    % smooth the histogram 
+    smooth_N = smooth(N,10,'rloess');
+    % find local minimus and prominance (derivative to a point)
+    [TF,P] = islocalmin(smooth_N);
+    % find the largest inflextion point
+    inflection_point = find(P==max(P));
+    % get inflection point as a number
+    E_sep_point = edges(inflection_point);
+    
+    
+    mkdir(fullfile(pwd,'output',this_exp));
+    progress_bar = 0;
+    for j = 1:num_imgs
+        progress_bar = progressbar_function(j,num_imgs,progress_bar,{'Writing data',char(this_exp_display)});
+        
+        amount_open(j) = sum(sum(E_imgs{j}<E_sep_point));
+        if export_images
+            E_mask = (E_imgs{j} < E_sep_point);
+
+            img_norm = double(imgs{j})/double(max(nonzeros(imgs{j})));
+
+            uint8_img = uint8(img_norm*255);
+            uint8_mask = uint8(E_mask*255);
+            uint8_E_img = uint8(E_imgs{j}/max(nonzeros(E_imgs{j}))*255);
+
+            rgb_img = cat(3,uint8_img,uint8_img,uint8_img);
+            rgb_mask = cat(3,uint8_mask,uint8_mask,uint8_mask);
+            rgb_E = cat(3,uint8_E_img,uint8_E_img,uint8_E_img);
+
+            out_label = labeloverlay(uint8(img_norm*255),E_mask,'Colormap','autumn','Transparency',0.75);
+            out_img = [rgb_img,rgb_mask;rgb_E,out_label];
+
+            imwrite(out_img,fullfile(pwd,'output',this_exp,[num2str(j) '.jpg']))
+        end
+        if isequal(j,num_imgs)
+            close_progressbar(progress_bar)
+        end
+    end
+    
+    time_points = 1:length(img_paths);
+    
+    grad_amount_open = gradient(amount_open)';
+    
+    header = ["time point","Amount open (pixels)","Norm open","Speed of closing","Norm close speed"];
+    
+    out = [time_points',amount_open',amount_open'/max(amount_open),grad_amount_open,grad_amount_open/max(abs(grad_amount_open))];
+    
+    out = cell2table(num2cell(out),'VariableNames',header);
+    
+    plot_data_simple(this_exp,edges,smooth_N,inflection_point,amount_open,fullfile(pwd,'output',this_exp,['_' this_exp '.png']))
+    writetable(out,fullfile(pwd,'output',this_exp,['_' this_exp '_data.csv']))
+        
+    clear amount_open imgs E_imgs
+    
 end
 
-large_E_array = imtile(E_imgs);
-[N,edges] = histcounts(nonzeros(large_E_array));
 
-smooth_N = smooth(N,10,'rloess');
+function plot_data_simple(this_exp,edges,smooth_N,inflection_point,amount_open,path_to_export)
 
-[TF,P] = islocalmin(smooth_N);
-
-inflection_point = find(P==max(P));
-
-e2 = edges(1:end-1);
-N_1000 = N>1000;
-
-figure;
-plot(e2,smooth_N,'b-',e2(inflection_point),smooth_N(inflection_point),'r*')
-
-E_sep_point = e2(inflection_point);
-
-for i = 1:length(img_paths)
+    e2 = edges(1:end-1);
+        
+    f = figure('Units','normalized','Position',[0,0.4,0.5,0.5]);
+    sgtitle(this_exp,'Interpreter','None');
+    subplot(1,2,1);
+    plot(e2,smooth_N,'b-',e2(inflection_point),smooth_N(inflection_point),'r*');
+    text(e2(inflection_point),smooth_N(inflection_point),{'inflex point', num2str(round(smooth_N(inflection_point)))});
+    title('Dual peak infereance');
+    xlabel('total entropy');
+    ylabel('measured entropy');
     
-    amount_open(i) = sum(sum(E_imgs{i}<E_sep_point));
+    subplot(1,2,2);
+    plot(amount_open/max(amount_open));
+    title('Normalized amount of open space');
+    xlabel('Hours');
+    
+    axis;
+    axis('square');
+    
+    saveas(f,path_to_export)
+    
+    close(f)
     
 end
 
-figure;
-plot(amount_open/max(amount_open))
 
-
-function progress_bar = progressbar_function(i,num_samples,progress_bar)
+function progress_bar = progressbar_function(i,num_samples,progress_bar,title)
 
 progress_ratio = i/num_samples;
 
 if isequal(progress_bar,0)
-    progress_bar = waitbar(progress_ratio,'Processing data');
+    progress_bar = waitbar(progress_ratio,title);
 else
-    progress_bar = waitbar(progress_ratio,progress_bar,'Processing data');
+    progress_bar = waitbar(progress_ratio,progress_bar,title);
 end
 
 end
